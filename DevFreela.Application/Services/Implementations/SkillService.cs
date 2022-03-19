8f30535e96db8575dedca6768be5754645c76b15
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-03-19 09:58:23 -0300

    install dapper and use it in skillService

--- DevFreela.Application/Services/Implementations/SkillService.cs
+++ DevFreela.Application/Services/Implementations/SkillService.cs
@@ -1,20 +1,29 @@
-﻿using DevFreela.Application.Services.Interfaces;
+﻿using Dapper;
+using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace DevFreela.Application.Services.Implementations
 {
     public class SkillService : ISkillService
     {
-        private readonly DevFreelaDbContext _dbContext;
-        public SkillService(DevFreelaDbContext dbContext)
+        private readonly DevFreelaDbContext _dbContext; 
+        private const string DevFreelaCs = "DevFreelaCs";
+        private readonly string _connectionString;
+        public SkillService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString(DevFreelaCs);
         }
         public IList<SkillViewModel> GetAll()
         {
-            var skills = _dbContext.Skills;
-            return skills.Select(s => new SkillViewModel(s.Id, s.Description)).ToList();
+            using var sqlConnection = new SqlConnection(_connectionString);
+            sqlConnection.Open();
+            var sql = @"SELECT Id, Description FROM Skills";
+
+            return sqlConnection.Query<SkillViewModel>(sql).ToList();
         }
     }
 }
