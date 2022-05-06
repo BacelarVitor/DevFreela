a393cf02a6ffbb7ac781a46507c2c79fd7e96e41
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-05-06 15:16:21 -0300

    first test

---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,21 @@
+using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using Xunit;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestIfProjectStartWorks()
+        {
+            var project = new Project("Test", "Desc de test", 1, 2, 190.0m);
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
