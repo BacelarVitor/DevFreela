4b0c0feaf0384793209800c2aaa3e97de7567f3d
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-05-06 17:12:59 -0300

    test scenarios status project

--- DevFreela.UnitTests/Core/Entities/ProjectTests.cs
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -7,7 +7,7 @@ namespace DevFreela.UnitTests.Core.Entities
     public class ProjectTests
     {
         [Fact]
-        public void TestIfProjectStartWorks()
+        public void ProjectStart_Executed_StatusIsInProgressAndStartedAtNotNull()
         {
             // Arrange
             var project = new Project("Test", "Desc de test", 1, 2, 190.0m);
@@ -15,10 +15,68 @@ namespace DevFreela.UnitTests.Core.Entities
             // Act
             project.Start();
 
-            //Assert 
+            // Assert 
             Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
             Assert.NotNull(project.StartedAt);
 
+        }
+
+        [Fact]
+        public void ProjectCancel_Executed_StatusIsCancelled()
+        {
+            // Arrange
+            var project = new Project("Test", "Desc de test", 1, 2, 100.0m);
+            project.Start();
+
+            // Act
+            project.Cancel();
+
+            // Assert
+            Assert.Equal(ProjectStatusEnum.Cancelled, project.Status);
+        }
+
+        [Fact]
+        public void ProjectFinish_Executed_StatusIsFinishedAndFinishedAtNotNull()
+        {
+            // Arrange
+            var project = new Project("Test", "Desc de test", 2, 4, 90.0m);
+            project.Start();
+
+            // Act
+            project.Finish();
+
+            // Assert
+            Assert.Equal(ProjectStatusEnum.Finished, project.Status);
+            Assert.NotNull(project.FinishedAt);
+        }
+
+        [Fact]
+        public void ProjectFinish_Executed_StatusIsCreatedAndFinishAtIsNull()
+        {
+            // Arrange
+            var project = new Project("Test", "Desc test", 4, 9, 203.58m);
+
+            // Act
+            project.Finish();
+
+            // Assert
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.FinishedAt);
+        }
+
+        [Fact]
+        public void ProjectStart_Executed_StatusIsCancelledAndStartAtIsNull()
+        {
+            // Arrange
+            var project = new Project("Test", "Desc test", 4, 9, 203.58m);
+            project.Cancel();
+
+            // Act
+            project.Start();
+
+            // Assert
+            Assert.Equal(ProjectStatusEnum.Cancelled, project.Status);
+            Assert.Null(project.StartedAt);
         }
     }
 }
